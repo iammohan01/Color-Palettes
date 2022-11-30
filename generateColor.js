fdf0d0d4166d8728bc10a1fa3af5d721a88c384a
iammohan01 <mohanasundaram7500@gmail.com>
2022-11-30 17:02:14 +0530

    created generated colors and palettes

--- generateColor.js
+++ generateColor.js
@@ -1,4 +1,5 @@
-const colorP = `<div class="colorPalette">
+const colorP = 
+`<div class="colorPalette">
     <div class="color1"></div>
     <div class="color2"></div>
     <div class="color3"></div>
@@ -8,28 +9,73 @@ const colorP = `<div class="colorPalette">
 
 
 // Complementary Color Scheme
-
-    let generatedColors = [[],[],[]] ;
-    var colorTemp = [] ;
+    let c1 = document.getElementById("c1");
+    let c2 = document.getElementById("c2");
+    let c3 = document.getElementById("c3");
+    let c4 = document.getElementById("c4");
+    let colorEle = document.querySelector(".paletteTab");
+    let generatedColors = [[],[],[],[]] ;
+    var colors = [] ;
     let colorNo = 0 ;
     let SecondColor;
     function generateColorsPalette(){
-        
+        generatedColors[0][0] = randomNumberGen(0,360);
+        generatedColors[0][1] = randomNumberGen(50,80) ;
+        generatedColors[0][2] = randomNumberGen(30,80) ;
 
+        generatedColors[1][0] = (generatedColors[0][0] + 180)%360 ;
+        generatedColors[1][1] = generatedColors[0][1] ;
+        generatedColors[1][2] = generatedColors[0][2] ;
+
+        generatedColors[2][0] = randomNumberGen(0,360);
+        generatedColors[2][1] = generatedColors[0][1];
+        generatedColors[2][2] = generatedColors[0][2] + 10 ;
+
+        generatedColors[3][0] = randomNumberGen(0,360);
+        generatedColors[3][1] = generatedColors[0][1] ;
+        generatedColors[3][2] = generatedColors[0][2] + 20 ;
+
+        colors.push(generatedColors) ;
+        createColorPalatteEle(generatedColors)
         
 
     }
-    function generateColor(){
-        //color range 0- 360 ;
-        // saturation 0-100 ;
-        // light 0 -100
-        let h = randomNumberGen(0,360);
-        let s = randomNumberGen(0,100);
-        let l = randomNumberGen(0,100);
-        colorTemp.push(h,s,l)
-        
+    function createColorPalatteEle(color){
+        let c=[] ;
+        let temp = `<div class="colorPalette">`
+        let count = 1 ;
+        color.forEach((i)=>{ 
+            let x = `hsl(${i[0]},${i[1]}%,${i[2]}%)` ;
+            c.push(x)
+            temp +=( `<div class='color${count++} cp' style="background-color:` + x + `" ></div>` );
+        })
+        temp += `</div>` ;
+        colorEle.innerHTML += temp ; 
     }
+
     function randomNumberGen(min,max){
         return Math.floor(Math.random() * (max - min + 1) + min) ;
     }
 
+
+
+function test(){
+    for(let i = 0 ; i < 100 ; i++){
+        generateColorsPalette()
+    }
+}
+
+test();
+   
+let lastContainer = colorEle.lastChild
+colorEle.addEventListener('scroll', () => {
+    const scrolled = colorEle.scrollTop+1000;
+    const elementPosition = lastContainer.offsetTop;
+    if(scrolled >= elementPosition){
+        test();
+        lastContainer = colorEle.lastChild ;
+    }
+})
+document.getElementsByClassName("cp").addEventListener("mouseover",()=>{
+    console.log("df");
+})
